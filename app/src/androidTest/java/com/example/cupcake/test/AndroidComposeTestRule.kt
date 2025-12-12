eaefecc13859ba7903a36a7634a48ae0913e862e
Zeba <saidzodazebida@gmail.com>
2025-12-12 23:47:58 +0300

    test7

---
+++ app/src/androidTest/java/com/example/cupcake/test/AndroidComposeTestRule.kt
@@ -0,0 +1,44 @@
+package com.example.cupcake.test
+
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import com.example.cupcake.ui.SelectOptionScreen
+import org.junit.Rule
+import org.junit.Test
+import com.example.cupcake.R
+
+
+
+class AndroidComposeTestRule {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+
+    @Test
+    fun selectOptionScreen_verifyContent() {
+        val flavors = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        val subtotal = "$100"
+
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subtotal, options = flavors)
+        }
+
+        flavors.forEach { flavor ->
+            composeTestRule.onNodeWithText(flavor).assertIsDisplayed()
+        }
+
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                subtotal
+            )
+        ).assertIsDisplayed()
+
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
+    }
+
+}
+}
--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -74,13 +74,63 @@ class CupcakeScreenNavigationTest {
         composeTestRule.onNodeWithStringId(R.string.next)
             .performClick()
     }
+    private fun performNavigateUp() {
+        val backText = composeTestRule.activity.getString(R.string.back_button)
+        composeTestRule.onNodeWithContentDescription(backText).performClick()
+    }
+    @Test
+    fun cupcakeNavHost_clickNextOnFlavorScreen_navigatesToPickupScreen() {
+        navigateToFlavorScreen()
+        composeTestRule.onNodeWithStringId(R.string.next)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Pickup.name)
+    }
 
+    @Test
+    fun cupcakeNavHost_clickBackOnFlavorScreen_navigatesToStartOrderScreen() {
+        navigateToFlavorScreen()
+        performNavigateUp()
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
 
+    @Test
+    fun cupcakeNavHost_clickCancelOnFlavorScreen_navigatesToStartOrderScreen() {
+        navigateToFlavorScreen()
+        composeTestRule.onNodeWithStringId(R.string.cancel)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
 
+    @Test
+    fun cupcakeNavHost_clickNextOnPickupScreen_navigatesToSummaryScreen() {
+        navigateToPickupScreen()
+        composeTestRule.onNodeWithText(getFormattedDate())
+            .performClick()
+        composeTestRule.onNodeWithStringId(R.string.next)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Summary.name)
+    }
 
+    @Test
+    fun cupcakeNavHost_clickBackOnPickupScreen_navigatesToFlavorScreen() {
+        navigateToPickupScreen()
+        performNavigateUp()
+        navController.assertCurrentRouteName(CupcakeScreen.Flavor.name)
+    }
 
+    @Test
+    fun cupcakeNavHost_clickCancelOnPickupScreen_navigatesToStartOrderScreen() {
+        navigateToPickupScreen()
+        composeTestRule.onNodeWithStringId(R.string.cancel)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
 
-
-
-
+    @Test
+    fun cupcakeNavHost_clickCancelOnSummaryScreen_navigatesToStartOrderScreen() {
+        navigateToSummaryScreen()
+        composeTestRule.onNodeWithStringId(R.string.cancel)
+            .performClick()
+        navController.assertCurrentRouteName(CupcakeScreen.Start.name)
+    }
 }
