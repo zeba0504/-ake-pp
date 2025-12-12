a9fb8326474d903259eeb1ca588fe3574ca870c8
Zeba <saidzodazebida@gmail.com>
2025-12-12 23:49:39 +0300

    finish

--- app/src/androidTest/java/com/example/cupcake/test/AndroidComposeTestRule.kt
+++ app/src/androidTest/java/com/example/cupcake/test/AndroidComposeTestRule.kt
@@ -2,20 +2,31 @@ package com.example.cupcake.test
 
 import androidx.activity.ComponentActivity
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import com.example.cupcake.data.OrderUiState
+import com.example.cupcake.ui.OrderSummaryScreen
 import com.example.cupcake.ui.SelectOptionScreen
 import org.junit.Rule
 import org.junit.Test
 import com.example.cupcake.R
-
-
+import com.example.cupcake.data.DataSource
+import com.example.cupcake.ui.StartOrderScreen
 
 class AndroidComposeTestRule {
     @get:Rule
     val composeTestRule = createAndroidComposeRule<ComponentActivity>()
 
+    private val fakeOrderUiState = OrderUiState(
+        quantity = 6,
+        flavor = "Vanilla",
+        date = "Wed Jul 21",
+        price = "$100",
+        pickupOptions = listOf()
+    )
 
     @Test
     fun selectOptionScreen_verifyContent() {
@@ -40,5 +51,50 @@ class AndroidComposeTestRule {
         composeTestRule.onNodeWithStringId(R.string.next).assertIsNotEnabled()
     }
 
-}
+    @Test
+    fun selectOptionScreen_optionSelected_NextButtonEnabled() {
+        val flavours = listOf("Vanilla", "Chocolate", "Hazelnut", "Cookie", "Mango")
+        val subTotal = "$100"
+
+        composeTestRule.setContent {
+            SelectOptionScreen(subtotal = subTotal, options = flavours)
+        }
+
+        composeTestRule.onNodeWithText("Vanilla").performClick()
+        composeTestRule.onNodeWithStringId(R.string.next).assertIsEnabled()
+    }
+
+    @Test
+    fun summaryScreen_verifyContentDisplay() {
+        composeTestRule.setContent {
+            OrderSummaryScreen(
+                orderUiState = fakeOrderUiState,
+                onCancelButtonClicked = {},
+                onSendButtonClicked = { _, _ -> },
+            )
+        }
+
+        composeTestRule.onNodeWithText(fakeOrderUiState.flavor).assertIsDisplayed()
+        composeTestRule.onNodeWithText(fakeOrderUiState.date).assertIsDisplayed()
+        composeTestRule.onNodeWithText(
+            composeTestRule.activity.getString(
+                R.string.subtotal_price,
+                fakeOrderUiState.price
+            )
+        ).assertIsDisplayed()
+    }
+
+    @Test
+    fun startOrderScreen_verifyContent() {
+        composeTestRule.setContent {
+            StartOrderScreen(
+                quantityOptions = DataSource.quantityOptions,
+                onNextButtonClicked = {}
+            )
+        }
+
+        DataSource.quantityOptions.forEach {
+            composeTestRule.onNodeWithStringId(it.first).assertIsDisplayed()
+        }
+    }
 }
