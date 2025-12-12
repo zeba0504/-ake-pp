12ecfdb14b64bedfab683d2fefe35f96cbbb02bc
Zeba <saidzodazebida@gmail.com>
2025-12-12 22:51:18 +0300

    Navigate to another app

--- app/src/main/java/com/example/cupcake/CupcakeScreen.kt
+++ app/src/main/java/com/example/cupcake/CupcakeScreen.kt
@@ -45,6 +45,9 @@ import com.example.cupcake.ui.SelectOptionScreen
 import androidx.compose.foundation.layout.fillMaxHeight
 import com.example.cupcake.data.DataSource.flavors
 import com.example.cupcake.ui.OrderSummaryScreen
+import android.content.Context
+import android.content.Intent
+
 /**
  * Composable that displays the topBar and displays back button if back navigation is possible.
  */
@@ -140,12 +143,14 @@ fun CupcakeApp(
                 )
             }
             composable(route = CupcakeScreen.Summary.name) {
+                val context = LocalContext.current
                 OrderSummaryScreen(
                     orderUiState = uiState,
                     onCancelButtonClicked = {
                         cancelOrderAndNavigateToStart(viewModel, navController)
                     },
                     onSendButtonClicked = { subject: String, summary: String ->
+                        shareOrder(context, subject = subject, summary = summary)
 
                     },
                     modifier = Modifier.fillMaxHeight()
@@ -163,4 +168,23 @@ private fun cancelOrderAndNavigateToStart(
     viewModel.resetOrder()
     navController.popBackStack(CupcakeScreen.Start.name, inclusive = false)
 }
+
+private fun shareOrder(context: Context, subject: String, summary: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
+}
+
+
+
+
+
 
