8254e784c1195b456d708a778578d7360625852a
Zeba <saidzodazebida@gmail.com>
2025-12-12 23:18:27 +0300

    test2

--- app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
+++ app/src/androidTest/java/com/example/cupcake/test/CupcakeScreenNavigationTest.kt
@@ -1,4 +1,26 @@
 package com.example.cupcake.test
 
+import androidx.activity.ComponentActivity
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import org.junit.Rule
+import androidx.navigation.testing.TestNavHostController
+import com.example.cupcake.CupcakeApp
+import androidx.compose.ui.platform.LocalContext
+import org.junit.Before
+import androidx.navigation.compose.ComposeNavigator
+
 class CupcakeScreenNavigationTest {
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+    private lateinit var navController: TestNavHostController
+
+    @Before
+    fun setupCupcakeNavHost() {
+        composeTestRule.setContent {
+            navController = TestNavHostController(LocalContext.current).apply {
+                navigatorProvider.addNavigator(ComposeNavigator())
+            }
+            CupcakeApp(navController = navController)
+        }
+    }
 }
